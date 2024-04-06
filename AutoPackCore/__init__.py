e1ab5691d3cf6420924da895da9d594947fb7773
kai_Ker <kai_Ker@buaa.edu.cn>
2024-04-06 22:47:50 +0800

    [Fix] Fix the wrongly import

--- AutoPackCore/__init__.py
+++ AutoPackCore/__init__.py
@@ -1,3 +1,3 @@
-from compiler import compile_java
-from jar_maker import make_jar
-from mainclass_detect import get_main_class
+from .compiler import compile_java
+from .jar_maker import make_jar
+from .mainclass_detect import get_main_class
