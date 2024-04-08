e87f4935226f4f435832eed813ba47b0d25a46b8
kai_Ker <kai_Ker@buaa.edu.cn>
2024-04-08 14:55:47 +0800

    [Feat] Draggable LineEdit

---
+++ GuiCore/draggable_line_edit.py
@@ -0,0 +1,26 @@
+from PyQt6.QtGui import QDragEnterEvent, QDragMoveEvent, QDropEvent
+from PyQt6.QtWidgets import QLineEdit
+
+
+class DraggableLineEdit(QLineEdit):
+    def __init__(self, parent=...):
+        super(DraggableLineEdit, self).__init__(parent)
+
+    def dragEnterEvent(self, a0: QDragEnterEvent) -> None:
+        if a0.mimeData().hasUrls():
+            a0.acceptProposedAction()
+        else:
+            a0.ignore()
+
+    def dragMoveEvent(self, e: QDragMoveEvent) -> None:
+        pass
+
+    def dropEvent(self, a0: QDropEvent) -> None:
+        mime_data = a0.mimeData()
+        if mime_data.hasUrls():
+            urls = mime_data.urls()
+            file_name = urls[0].toLocalFile()
+            self.setText(file_name)
+            a0.accept()
+        else:
+            a0.ignore()
